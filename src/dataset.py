a6ac0ed5b22251fb7df5024ca148d4017f92c5e8
Harsh Prajapati <145095712+Harsh-Prajapati54@users.noreply.github.com>
2026-09-25 23:40:07 +0530

    Refactor dataset loading function to separate training and validation data preparation

--- src/dataset.py
+++ src/dataset.py
@@ -1,11 +1,32 @@
 from datasets import load_from_disk
-# loads a dataset from disk 
-dataset = load_from_disk(r"C:\LLM from Scratch\Data")
 
-# joining all row as an single text 
 
-dataset_text = " ".join(dataset["train"]['text'][:200000])
+def load_dataset():
+    
+    # ============================================================
+    # Load dataset -- Tiny stories 
+    # ============================================================
+    
+    dataset = load_from_disk(r"C:\LLM from Scratch\Data")
 
-data = dataset_text
+    # ============================================================
+    # Prepare training and validation text
+    # ============================================================
 
-print(f"data sample:  {data[:500]}")
+    dataset_text_train = "".join(dataset["train"]['text'][:200000])
+    dataset_text_val = "".join(dataset["validation"]['text'][:21990]) 
+  
+    train_data = dataset_text_train
+    val_data= dataset_text_val
+    
+    return train_data,val_data
+
+if __name__ =="__main__":
+    
+    train_data , val_data = load_dataset()
+    # ============================================================
+    # Check loaded data
+    # ===========================================================
+    print(f"train data sample:\n  {train_data[:500]}")
+    print("=" * 100)
+    print(f"val data sample :\n {val_data[:500]}")
